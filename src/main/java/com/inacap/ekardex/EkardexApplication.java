edc5cab06d7c734ade4acb74ba9ec99501eca702
david <davidw.rq@gmail.com>
2018-04-10 13:53:49 -0300

    initial commit

---
+++ src/main/java/com/inacap/ekardex/EkardexApplication.java
@@ -0,0 +1,12 @@
+package com.inacap.ekardex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkardexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkardexApplication.class, args);
+	}
+}
